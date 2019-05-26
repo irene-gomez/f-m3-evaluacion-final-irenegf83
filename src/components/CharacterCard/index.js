5b2da23d512f9fd53a1de04bfe8a3b7281d2dfa8
irenegf83 <irene@irenegf.com>
2019-05-26 21:15:25 +0200

    Add fonts with @font-face

--- src/components/CharacterCard/index.js
+++ src/components/CharacterCard/index.js
@@ -6,9 +6,7 @@ import './styles.scss';
 const CharacterCard = props => {
     return (
         <div className="card">
-            <div className="card__image">
-                <img  src={props.image} alt={props.name} />
-            </div>
+            <img className="card__image" src={props.image} alt={props.name} />
             <p className="card__name">{props.name}</p>
             <p className="card__house">{props.house ? props.house : 'Sin casa'}</p>
         </div>
