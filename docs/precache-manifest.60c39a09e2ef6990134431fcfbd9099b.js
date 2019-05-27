f559071b68c0c9f7460de733dc03445ece99ac01
irenegf83 <irene@irenegf.com>
2019-05-27 10:19:50 +0200

    Create docs directory

---
+++ docs/precache-manifest.60c39a09e2ef6990134431fcfbd9099b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "829bb00fe62e592a4ea2699d2002968b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d201097d07a0f50137a",
+    "url": "/static/css/main.56716531.chunk.css"
+  },
+  {
+    "revision": "97f7ebd97d1653e16046",
+    "url": "/static/js/2.0276fec2.chunk.js"
+  },
+  {
+    "revision": "7d201097d07a0f50137a",
+    "url": "/static/js/main.49a945a7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "37ba38a1068c5d3223e7ad63ac64df93",
+    "url": "/static/media/HARRYP.37ba38a1.ttf"
+  },
+  {
+    "revision": "7db67e004e72929da3c964911dd5aff6",
+    "url": "/static/media/HARRYP.7db67e00.woff2"
+  },
+  {
+    "revision": "e3f9ab4bf9ffe0e33b94298efbc983b4",
+    "url": "/static/media/HARRYP.e3f9ab4b.woff"
+  },
+  {
+    "revision": "28e7b29b1e26021d4884f02567862f18",
+    "url": "/static/media/gryffindor_flag.28e7b29b.png"
+  },
+  {
+    "revision": "bf91b6bc4239d37ae68f48afcaaf096b",
+    "url": "/static/media/hufflepuff_flag.bf91b6bc.png"
+  },
+  {
+    "revision": "51ebf142c8d9ac399fb7e24939e32a6e",
+    "url": "/static/media/main-backgorund.51ebf142.jpg"
+  },
+  {
+    "revision": "5b2a0e27774db390b4b2f904a3ba8cc8",
+    "url": "/static/media/ravenclaw_flag.5b2a0e27.png"
+  },
+  {
+    "revision": "a93d58b365da2d8f0bb348f17a6656c5",
+    "url": "/static/media/slytherin_flag.a93d58b3.png"
+  }
+]);
